7aee1cd53f3911293c1f270bdd22dd709d03e3e4
georgeiliadis91 <george.iliadis.91@gmail.com>
2022-04-01 00:14:01 +0300

    added profile img on return data

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -16,7 +16,7 @@ module.exports = (plugin) => {
     const user = await strapi.entityService.findOne(
       "plugin::users-permissions.user",
       ctx.state.user.id,
-      { populate: ["profile_data"] }
+      { populate: ["profile_data","profile_data.profile_img"] }
     );
 
     ctx.body = sanitizeOutput(user);
@@ -25,7 +25,7 @@ module.exports = (plugin) => {
   plugin.controllers.user.find = async (ctx) => {
     const users = await strapi.entityService.findMany(
       "plugin::users-permissions.user",
-      { ...ctx.params, populate: ["profile_data"] }
+      { ...ctx.params, populate: ["profile_data","profile_data.profile_img"] }
     );
 
     ctx.body = users.map((user) => sanitizeOutput(user));
