0a4ea4d6a83770ef714f328f7a4c1dcd9cbab470
georgeiliadis91 <george.iliadis.91@gmail.com>
2022-04-03 22:03:51 +0300

    custom user endpoint for permission data

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -9,6 +9,64 @@ module.exports = (plugin) => {
     return sanitizedUser;
   };
 
+  const sanitizeUserWithPermissions = (ctx,userData) => {
+  const {permissions} = userData.profile_data;
+
+    let isAuthenticated = false;
+
+    if(ctx.state.user){
+      isAuthenticated = true;
+    }
+
+    // if me return empty object since its private to self
+    if(permissions.everything ==='private'){
+     return {}
+    }
+
+    // if all return all the fields
+    if(permissions.everything ==='public'){
+      return userData;
+    }
+
+    if(permissions.everything ==='custom'){
+
+      const newUser = {};
+
+      Object.keys(permissions).forEach((key) => {
+        const fieldKey = permissions[key];
+
+        // skip iteration for everything key
+        if(key === 'everything'){
+          return;
+        }
+
+        if(fieldKey ==='authed' || fieldKey ==='public' ){
+          newUser[key]=userData[key];
+        }
+        // if is profile_data object
+        if(key ==='profile_data'){
+          const profileData = {};
+
+          Object.keys(permissions.profile_data).forEach((key) => {
+
+            const keyPermission = permissions.profile_data[key]
+
+            if(keyPermission ==='authed' || keyPermission ==='all' ){
+              profileData[key]=userData.profile_data[key];
+            }
+
+          })
+
+          newUser['profile_data']=profileData;
+        }
+      });
+
+      return {newUser};
+    }
+
+    return userData;
+  }
+
   plugin.controllers.user.me = async (ctx) => {
     if (!ctx.state.user) {
       return ctx.unauthorized();
@@ -23,12 +81,31 @@ module.exports = (plugin) => {
   };
 
   plugin.controllers.user.find = async (ctx) => {
+
     const users = await strapi.entityService.findMany(
       "plugin::users-permissions.user",
       { ...ctx.params, populate: ["profile_data","profile_data.profile_img"] }
     );
+    //TODO : sanitize output according to user permission object
 
     ctx.body = users.map((user) => sanitizeOutput(user));
+  };
+
+  plugin.controllers.user.findOne = async (ctx) => {
+
+
+
+    const user = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+      ctx.params.id,
+      { ...ctx.params, populate: ["profile_data","profile_data.profile_img"] },
+    );
+
+
+    //TODO : sanitize output according to user permission object
+
+    ctx.body = sanitizeOutput(sanitizeUserWithPermissions(ctx,user));
+
   };
 
   return plugin;
